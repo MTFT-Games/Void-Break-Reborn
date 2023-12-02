78522f59ba28a321b17598b27b290d6afd3b41d8
Noah Emke <noahremke@gmail.com>
2023-12-02 17:35:45 -0500

    Spawned background Wont render yet, need camera Made Background marker and spawn_core system

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,20 @@ use bevy::prelude::*;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins) // TODO: Look through defaults and disable things I don't need.
+        .add_systems(Startup, spawn_core)
         .run();
 }
+
+fn spawn_core(mut commands: Commands, assets: Res<AssetServer>) {
+    commands.spawn((
+        SpriteBundle {
+            texture: assets.load("purple_nebula_4_repeated.png"),
+            ..Default::default()
+        },
+        Background,
+    ));
+}
+
+// Not really needed for anything yet but might be useful later
+#[derive(Component)]
+struct Background;
