01d6e5b8e3991046ee3675daa4debb9a2f0a37ad
Noah Emke <noahremke@gmail.com>
2023-12-03 00:57:32 -0500

    Added delta time Second thought: why did i do that?

--- src/main.rs
+++ src/main.rs
@@ -50,25 +50,25 @@ struct Player;
 // Todo: Make it all delta time based
 fn player_controller(
     mut transform: Query<&mut Transform, With<Player>>,
-    keyboard: Res<Input<KeyCode>>,
+    keyboard: Res<Input<KeyCode>>, time: Res<Time>
 ) {
     // If there are ever more than one player, something has gone very wrong
     let mut player_transform = transform.single_mut();
     let forward = player_transform.local_y();
 
     if keyboard.pressed(KeyCode::W) {
-        player_transform.translation += forward;
+        player_transform.translation += forward * 200.0 * time.delta_seconds();
     }
     // TODO: lock reverse behind an upgrade later
     if keyboard.pressed(KeyCode::S) {
-        player_transform.translation -= forward;
+        player_transform.translation -= forward * 200.0 * time.delta_seconds();
     }
 
     use std::f32::consts::PI;
     if keyboard.pressed(KeyCode::A) {
-        player_transform.rotate_local_z(PI / 30.0);
+        player_transform.rotate_local_z(PI * time.delta_seconds());
     }
     if keyboard.pressed(KeyCode::D) {
-        player_transform.rotate_local_z(PI / -30.0);
+        player_transform.rotate_local_z(PI * -time.delta_seconds());
     }
 }
