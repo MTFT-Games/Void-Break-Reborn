3740e7b8fdfcbabadb5acc99ce0e3a3bba6acecf
Noah Emke <noahremke@gmail.com>
2023-12-02 23:31:04 -0500

    Added basic movement Just hardcoded wasd 1 pixel per frame for now

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins) // TODO: Look through defaults and disable things I don't need.
         .add_systems(Startup, spawn_core)
+        .add_systems(Update, player_controller)
         .run();
 }
 
@@ -43,3 +44,39 @@ struct PlayerBundle {
 
 #[derive(Component)]
 struct Player;
+
+fn player_controller(
+    mut transform: Query<&mut Transform, With<Player>>,
+    keyboard: Res<Input<KeyCode>>,
+) {
+    let mut player_transform = transform.single_mut();
+    // Todo: Make it all delta time based
+    if keyboard.pressed(KeyCode::W) {
+        player_transform.translation += Vec3 {
+            x: 0.0,
+            y: 1.0,
+            z: 0.0,
+        };
+    }
+    if keyboard.pressed(KeyCode::A) {
+        player_transform.translation += Vec3 {
+            x: -1.0,
+            y: 0.0,
+            z: 0.0,
+        };
+    }
+    if keyboard.pressed(KeyCode::S) {
+        player_transform.translation += Vec3 {
+            x: 0.0,
+            y: -1.0,
+            z: 0.0,
+        };
+    }
+    if keyboard.pressed(KeyCode::D) {
+        player_transform.translation += Vec3 {
+            x: 1.0,
+            y: 0.0,
+            z: 0.0,
+        };
+    }
+}
